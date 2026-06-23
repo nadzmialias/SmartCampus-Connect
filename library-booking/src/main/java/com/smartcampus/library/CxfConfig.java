3b57b8f405ad3b575691b98a8d08859a04f030d0
durraniafiq <durraniafiq@gmail.com>
2026-06-23 10:19:26 +0800

    Add Library SOAP Service with WSDL and fault generation (Requirement R8)

---
+++ library-booking/src/main/java/com/smartcampus/library/CxfConfig.java
@@ -0,0 +1,28 @@
+package com.smartcampus.library;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.xml.ws.Endpoint;
+
+@Configuration
+public class CxfConfig {
+
+    @Autowired
+    private Bus bus;
+
+    @Autowired
+    private LibrarySoapService librarySoapService;
+
+    @Bean
+    public Endpoint endpoint() {
+        EndpointImpl endpoint = new EndpointImpl(bus, librarySoapService);
+        
+        // This publishes our service WSDL at http://localhost:8083/services/booking?wsdl
+        endpoint.publish("/booking"); 
+        
+        return endpoint;
+    }
+}
